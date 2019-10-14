0a928a7670a0af0aad77c939104eda440da4fbfe
Bethune <jbethune777@gmail.com>
2019-10-14 22:07:11 +0900

    Adding week 3 exercises and homework

---
+++ week3/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Week 3 Exercises: Functions
+
+# EXERCISE 1:
+
+# Implement FizzBuzz using function(s) with Clean Code standards
+
+# -----FIZZBUZZ CODE HERE-----
+
+
+# -----END FIZBUZZ CODE-----
+
+# EXERCISE 2:
+
+# Review the payroll.java code in Listing 3-4 in the book
+
+# Re-implement it as clean Python code. See Listing 3-5 for guidance.
+
+# -----PAYROLL CODE HERE-----
+
+
+# -----END PAYROLL CODE-----
